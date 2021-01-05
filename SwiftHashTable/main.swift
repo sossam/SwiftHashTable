882289dc2c589b48a0b34bf69493dd524d5306ae
sossam <heart_b_e@naver.com>
2021-01-05 16:15:25 +0900

    ✨ HashTable 구현

--- SwiftHashTable/main.swift
+++ SwiftHashTable/main.swift
@@ -7,5 +7,33 @@
 
 import Foundation
 
-print("Hello, World!")
+var hashTable: [String?] = .init(repeating: nil, count: 3)
+
+func hash(key: Int) -> Int {
+    return key % 3
+}
+
+func updateValue(_ value: String, forKey key: String) {
+    guard let key = UnicodeScalar(key)?.value else { return }
+    let hashAddress = hash(key: Int(key))
+    hashTable[hashAddress] = value
+}
+
+func getValue(forKey key: String) -> String? {
+    guard let key = UnicodeScalar(key)?.value else { return nil }
+    let hashAddress = hash(key: Int(key))
+    return hashTable[hashAddress]
+}
+
+
+
+updateValue("재석", forKey: "유")
+updateValue("명수", forKey: "박")
+updateValue("소들", forKey: "김")
+
+
+getValue(forKey: "유")
+getValue(forKey: "박")
+getValue(forKey: "유")
+
 
